5fbc1952d74df909cd26e7f56e95b12736ae7bd2
jesua <jesua@DAV-PC>
2020-11-17 21:36:43 -0600

    Agregamos el clasificador Knn

--- src/clasificadores/K_NN.java
+++ src/clasificadores/K_NN.java
@@ -8,6 +8,8 @@ package clasificadores;
 import Interfaces.ClasificadorSupervisado;
 import data.Patron;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
@@ -15,23 +17,46 @@ import java.util.ArrayList;
  * @author jesua
  */
 public class K_NN implements ClasificadorSupervisado{
-    private ArrayList<String> clases;
+    private ArrayList<String> clases, aux;
     private ArrayList<Patron> instancias;
-    private int k;
-    
+    private int k, max;
+    private int mayor[];
     public K_NN(int k) {
         this.clases = new ArrayList<>();
         this.k = k;
     }
     @Override
     public void entrenar(ArrayList<Patron> instancias) {
+        aux = new ArrayList();
+       
+        for(int x=0; x<instancias.size(); x++){
+                aux.add(instancias.get(x).getClase());
+        }
+        
+       Set<String> hs = new HashSet<String>(aux);
+       aux.clear();
+       aux.addAll(hs);
+       
        this.instancias = (ArrayList<Patron>) instancias.clone();
         // generamos un arraylist con las clases que se estan trabajando
         for(Patron p: instancias){
            if(!this.clases.contains(p.getClase())){
                 this.clases.add(p.getClase());
-           }
+           } 
        }
+        mayor = new int[aux.size()];
+          //contamos el numero de instancias por clase para obtener nuestro k maximo
+        for(int x=0; x<aux.size();x++){
+            for(int y=0; y<instancias.size(); y++){
+                if(instancias.get(y).getClase().equals(aux.get(x))){
+                   mayor[x]++; 
+                   
+                }
+                if(mayor [x] > max) {
+                    max = mayor[x];
+                }
+            }
+         }
     }
 
     @Override
@@ -53,20 +78,23 @@ public class K_NN implements ClasificadorSupervisado{
     
     public void clasificar(Patron patron) {
         
-        if(k!=0){
+        if(k!=0 && k<max){
          //sort funciona como algoritmo de ordenamiento burbuja
         instancias.sort((a, b) -> new Double(a.calcularDistancia(patron)).compareTo(new Double(b.calcularDistancia(patron))));
         int contador[] = new int[this.clases.size()];
-        // clasificar en base al numero de vecinos
+        // clasificacion de acuerdo a los k-vecinos
         for(Patron aux: this.instancias){
             int i = this.clases.indexOf(aux.getClase());
             contador[i]++;
             if(contador[i]==this.k){
-                // clasificar
+                // clasificacion
                 patron.setClaseResultante(this.clases.get(i));
                 break;
             }
         }
-      }   
+      }else{
+         System.out.println("El número k de vecinos introducido no esta definido");
+         System.out.close();
+        }
     }
 }
