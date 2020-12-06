0740148ec7975635752fc1d15f78fffb295d5573
jesua <jesua@DAV-PC>
2020-12-06 10:31:36 -0600

    cambios

---
+++ src/Interfaces/Clusterizacion.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+import java.util.ArrayList;
+import data.Patron;
+
+public interface Clusterizacion {
+
+    public abstract void clusterizar(ArrayList<Patron> instancias);
+
+}
---
+++ src/clasificadores/CMeans.java
@@ -0,0 +1,19 @@
+package clasificadores;
+
+import java.util.ArrayList;
+
+import Interfaces.Clusterizacion;
+import data.Patron;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CMeans implements Clusterizacion{
+
+    }
+
+@Override
+public void clusterizar(ArrayList<Patron> instancias) {
+	
+	
+}
